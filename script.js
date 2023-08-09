851d98b828502a1c3e2c07eee0a3a50fe183118d
Uvindu Rajapakshe <118661330+UvinduBro@users.noreply.github.com>
2023-08-09 12:27:09 +0000

    new file:   images/kite1.png 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const kite = document.getElementById('kite');
-const kiteSelect = document.getElementById('kite-select');
 let angle = 0;
 let altitude = 0;
 
 function updateKitePosition() {
     kite.style.transform = `translateX(-50%) rotate(${angle}deg) translateY(-${altitude}px)`;
 }
-
-kiteSelect.addEventListener('change', (event) => {
-    const selectedKite = event.target.value;
-    kite.src = selectedKite;
-});
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowLeft') {
