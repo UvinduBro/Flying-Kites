aad3ea60ba9d5703b643bc21f10e266961af4cea
Uvindu Rajapakshe <118661330+UvinduBro@users.noreply.github.com>
2023-08-09 11:06:41 +0000

    new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,23 @@
+const kite = document.getElementById('kite');
+let angle = 0;
+let altitude = 0;
+
+function updateKitePosition() {
+    kite.style.transform = `translateX(-50%) rotate(${angle}deg) translateY(-${altitude}px)`;
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft') {
+        angle -= 10;
+    } else if (event.key === 'ArrowRight') {
+        angle += 10;
+    } else if (event.key === 'ArrowUp') {
+        altitude += 10;
+    } else if (event.key === 'ArrowDown') {
+        altitude -= 10;
+    }
+    
+    updateKitePosition();
+});
+
+updateKitePosition();
