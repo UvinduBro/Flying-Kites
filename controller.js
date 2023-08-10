71469e49a0b2fe5dff6f3edf4d9fb582778723bb
Lakshitha <Lakshithagamer@gmail.com>
2023-08-10 21:57:14 +0530

    Fixed button functionality

--- controller.js
+++ controller.js
@@ -1,27 +1,27 @@
-const kite = document.getElementById('kite');
+const kiteForButton = document.getElementById('kite');
 const leftButton = document.getElementById('left');
 const rightButton = document.getElementById('right');
 const upButton = document.getElementById('up');
 const downButton = document.getElementById('down');
 
-let angle = 0;
-let altitude = 0;
+let angleForButton = 0;
+let altitudeForButton = 0;
 
 function updateKitePosition() {
-    kite.style.transform = `translateX(-50%) rotate(${angle}deg) translateY(-${altitude}px)`;
+    kiteForButton.style.transform = `translateX(-50%) rotate(${angleForButton}deg) translateY(-${altitudeForButton}px)`;
 }
 
 function handleButtonPress(direction) {
     if (direction === 'left') {
-        angle -= 10;
+        angleForButton -= 10;
     } else if (direction === 'right') {
-        angle += 10;
+        angleForButton += 10;
     } else if (direction === 'up') {
-        altitude += 10;
+        altitudeForButton += 10;
     } else if (direction === 'down') {
-        altitude -= 10;
+        altitudeForButton -= 10;
     }
-    
+
     updateKitePosition();
 }
 
